cc368481144fdbf587d27989a41cb1728fd33086
Cal Chengqi Fang <chengqi@uchicago.edu>
2022-01-11 12:39:54 -0600

    Create 01_prep.R

---
+++ 01_prep.R
@@ -0,0 +1,83 @@
+library(Matrix)
+
+# Set working directory and read in the four datasets
+setwd("~/Box Sync/Name Identification Project/US Names")    # Please change this to your path
+
+cntr <- read.fwf("data/srnmcntr.txt", c(12,2,6), col.names=c("surname","country","freq"), na.strings = c())
+fb <- read.fwf("data/srnmfb.txt", c(12,8), col.names=c("surname","freq"))
+ntv <- read.fwf("data/srnmntv.txt", c(12,8), col.names=c("surname","freq"))
+terr <- read.fwf("data/srnmterr.txt", c(12,8), col.names=c("surname","freq"))
+
+
+# Create vector of unique surnames and update factor levels for all objects
+surnames <- sort(union(fb$surname, terr$surname))      # Take out all surnames appearing in foreign born or us territory files
+N_surnames <- length(surnames)
+
+cntr$surname <- factor(cntr$surname, levels=surnames)
+fb$surname <- factor(fb$surname, levels=surnames)
+ntv$surname <- factor(ntv$surname, levels=surnames)
+terr$surname <- factor(terr$surname, levels=surnames)
+
+# Create vector of country codes 
+countrynames <- sort(unique(cntr$country))
+N_countrynames <- length(countrynames)
+
+cntr$country <- factor(cntr$country, levels=countrynames)
+
+# Create US born column sparse matrix
+ntvmat <- sparseMatrix(
+  i = as.integer(ntv$surname[!is.na(ntv$surname)]),
+  j = rep(1,sum(!is.na(ntv$surname))),
+  x = as.integer(ntv$freq[!is.na(ntv$surname)]),
+  dims = c(N_surnames,1))
+rownames(ntvmat) = surnames
+colnames(ntvmat) = "US"
+
+# Create US terr column sparse matrix
+terrmat <- sparseMatrix(
+  i = as.integer(terr$surname),
+  j = rep(1,nrow(terr)),
+  x = as.integer(terr$freq),
+  dims = c(N_surnames,1))
+rownames(terrmat) = surnames
+colnames(terrmat) = "Territory"
+
+# Create foreign born column sparse matrix
+fbmat <- sparseMatrix(
+  i = as.integer(fb$surname),
+  j = rep(1,nrow(fb)),
+  x = as.integer(fb$freq),
+  dims = c(N_surnames,1))
+rownames(fbmat) = surnames
+colnames(fbmat) = "Foreign"
+
+# Create N_surnames by N_countrynames sparse matrix
+cntrmat <- sparseMatrix(
+  i = as.integer(cntr$surname),
+  j = as.integer(cntr$country),
+  x = as.integer(cntr$freq),
+  dims = c(N_surnames, N_countrynames))
+rownames(cntrmat) = surnames
+colnames(cntrmat) = countrynames
+
+
+# What's the matter with "Nguyen"?
+sort(prop.table(cntrmat[which.max(rowSums(cntrmat)), ]))
+
+# What about "Lauderdale"?
+cntrmat[grep("LAUDERDALE", surnames), ]
+
+# Combine US, Terr, and Foreign matrices
+denommat <- cbind(ntvmat,terrmat,fbmat)
+
+# Create country proportion matrix
+gc()    # following command requires ~ 12GB working memory
+cntrmatprop <- sweep(cntrmat, 1, fbmat[, 1] + terrmat[, 1], FUN="/")
+gc()
+
+# Save objects
+save(cntrmat, cntrmatprop, 
+     fbmat, ntvmat, terrmat, 
+     surnames, N_surnames,
+     countrynames, N_countrynames,
+     file="data/AllData.Rdata")
