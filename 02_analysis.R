28136050608803ecd4ab6e5f99d8a669c4d71497
Cal Chengqi Fang <chengqi@uchicago.edu>
2022-01-12 12:35:17 -0600

    Rotate matrices and update the output command

--- 02_analysis.R
+++ 02_analysis.R
@@ -26,7 +26,7 @@ fb_estimates <- data.frame(
   row.names = countrynames
 )
 
-
+# Create a dataframe of foreign-born estmiates of exclusive names
 for (i in 1:N_countrynames){
   tmp <- constructCountryNameList(countrynames[i])
   tmp2 <- tmp$fbprop == 1
@@ -36,16 +36,31 @@ for (i in 1:N_countrynames){
   fb_estimates$fb_among_exclusive_surnames[i] <- sum(tmp4*total) / sum(total)
   fb_estimates$exclusive_surnames[i] <- length(tmp4)
   fb_estimates$count_with_exclusive_surnames[i] <- sum(total)
-  print(fb_estimates[i, ])
+  # print(fb_estimates[i, ])
 }
 
-
-
-tmp <- fb_estimates
-tmp[,1] <- round(100*tmp[, 1], 1)
-tmp <- tmp[order(tmp[, 3], decreasing=TRUE), ]
-write.csv(tmp, file="data/ForeignBornAmongExclusiveSurnames.csv")
+# Save the file just in case
+# tmp <- fb_estimates
+# tmp[,1] <- round(100*tmp[, 1], 1)
+# tmp <- tmp[order(tmp[, 3], decreasing=TRUE), ]
+# write.csv(tmp, file="data/ForeignBornAmongExclusiveSurnames.csv")
 
 #plot(total,tmp4,pch=16,col=rgb(0,0,0,0.2),cex=sqrt(total/100),
 #     xlab="Total with Surname",ylab="Fraction Foreign Born with Surname",
 #     main="Exclusive Surnames")
+
+# Rotate all matrices to match with the method doc
+cntrmat <- t(cntrmat)
+cntrmatprop <- t(cntrmatprop)
+fbmat <- t(fbmat)
+ntvmat <- t(ntvmat)
+terrmat <- t(terrmat)
+denommat <- t(denommat)
+
+# Update the data file
+save(cntrmat, cntrmatprop, 
+     fbmat, ntvmat, terrmat, denommat,
+     surnames, N_surnames,
+     countrynames, N_countrynames,
+     fb_estimates,
+     file="data/AllData2.Rdata")
