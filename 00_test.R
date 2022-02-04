9ba48cf7640bd5940f6026bc175487bcd325a77c
Cal Chengqi Fang <chengqi@uchicago.edu>
2022-02-03 21:49:32 -0600

    Create TH dataframes for tests.

--- 00_test.R
+++ 00_test.R
@@ -4,18 +4,47 @@ library(Matrix)
 setwd("~/Box Sync/Name Identification Project/US Names")    # Please change this to your path
 
 # Load in the data
-load(file="data/AllData2.Rdata")
+load(file="data/AllData.Rdata")
+
+# Create test dataset first 
+# Use TH ("Thailand") for this test by the way
+library(tidyverse)
+test1 <- cntr %>% 
+  filter(country == "TH")
+sur_test <- sort(unique(test1$surname))
+
+test2 <- us %>% 
+  filter(surname %in% sur_test)
+test3 <- fb %>% 
+  filter(surname %in% sur_test)
+
+test4 <- cntr %>% 
+  filter((country != "TH") & 
+           (surnames %in% sur_test))
 
 # Test the convergence loop on several values
-for (i in countrynames){
+for (i in countrynames) 
+{
   pi_ij <- Y_ij / phi_i * (Y_0j + sum(Y_ij)) / sum(Y_ij / phi_i)
-    for (j in surnames){
-      Y_ij_frame = Y_ij_frame %>% 
-        filter(Y_ij > 0)
-      phi_i = sum(Y_ij / pi_ij) / length(Y_ij)
-    }
+  for (j in surnames)
+  {
+    Y_ij_frame = Y_ij_frame %>% 
+      filter(Y_ij > 0)
+    phi_i = sum(Y_ij / pi_ij) / length(Y_ij)
+  }
 }
   
+
+pi_vietnam_chew <- cntr["vietnam", "Chew"] / fb_estimates["vietnam"] * (ntv["Chew"] + terr["Chew"] + sum(cntr["Chew"])) / 
+
+
+
+
+
+
+
+
+
 
 
 
@@ -35,15 +64,6 @@ cntr %>%
 cntr %>% 
   filter(country == "YQ") 
 
-""
-
-
-library(tidyverse)
-cntr %>% 
-
-
-plot(cntr$freq)  
-
 
 cntr %>% 
   filter(surname == "A           ") %>% 
@@ -52,18 +72,6 @@ cntr %>%
 terr %>% 
   filter(surname == "ZHAO        ") %>% 
   summarise(sum = sum(freq))
-
-library(haven)
-SSA_raw <- read_dta("data/usa_00027.dat")
-
-
-readLines("data/usa_00027.dat", 
-          n=10)
-
-
-
-
-
 
 
 
