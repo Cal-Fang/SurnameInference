e4930ccb0598e53c5a7d6cf7f157ac0b3f6408e3
Cal Chengqi Fang <chengqi@uchicago.edu>
2022-02-07 14:18:51 -0600

    Change some details.

--- 01_prep.R
+++ 01_prep.R
@@ -1,3 +1,7 @@
+# Clear out the history
+rm(list = ls())
+
+# Read in the libraries needed
 library(Matrix)
 library(tidyverse)
 
@@ -17,33 +21,35 @@ fb <- cntr %>%
   summarise(freq = sum(freq))
 
 # Also need to combine ntv and terr cuz it just makes more sense
-us <- ntv %>% 
-  merge(terr, by="surname", all=TRUE) %>% 
-  mutate(freq.x = replace_na(freq.x, 0),
-         freq.y = replace_na(freq.y, 0),
-         freq = freq.x + freq.y) %>% 
-  select(-c(freq.x, freq.y))
+# us <- ntv %>% 
+#   merge(terr, by="surname", all=TRUE) %>% 
+#   mutate(freq.x = replace_na(freq.x, 0),
+#          freq.y = replace_na(freq.y, 0),
+#          freq = freq.x + freq.y) %>% 
+#   select(-c(freq.x, freq.y))
 
-# Apply the data cleaning result onto the cntr dataset
+# Apply the data cleaning result onto the cntr dataset and create the new country2 variable
 cntr <- cntr %>% 
   merge(census_Asian[, c("code", "code3")], by.x="country", by.y="code", all.x=TRUE) %>% 
   mutate(country2 = ifelse(is.na(code3), "Other", code3)) %>% 
   select(-c(code3))
 
 # Create vector of unique surnames and update factor levels for all objects
-surnames <- sort(union(fb$surname, terr$surname))      # Take out all surnames appearing in foreign born or us territory files
-N_surnames <- length(surnames)
+surnames <- sort(union(fb$surname, ntv$surname))      # Originally take out the union of fb and terr. 
+N_surnames <- length(surnames)                        # I think it makes more sense to take the union of fb and ntv.
 
 cntr$surname <- factor(cntr$surname, levels=surnames)
 fb$surname <- factor(fb$surname, levels=surnames)
+
 ntv$surname <- factor(ntv$surname, levels=surnames)
 terr$surname <- factor(terr$surname, levels=surnames)
+us$surname <- factor(us$surname, levels=surnames)
 
 # Create vector of country codes 
-countrynames <- sort(unique(cntr$country))
+countrynames <- sort(unique(cntr$country2))
 N_countrynames <- length(countrynames)
 
-cntr$country <- factor(cntr$country, levels=countrynames)
+cntr$country2 <- factor(cntr$country2, levels=countrynames)
 
 # Create US born column sparse matrix
 ntvmat <- sparseMatrix(
@@ -61,7 +67,7 @@ terrmat <- sparseMatrix(
   x = as.integer(terr$freq),
   dims = c(N_surnames,1))
 rownames(terrmat) = surnames
-colnames(terrmat) = "Territory"
+colnames(terrmat) = "US Territory"
 
 # Create foreign born column sparse matrix
 fbmat <- sparseMatrix(
@@ -75,26 +81,19 @@ colnames(fbmat) = "Foreign"
 # Create N_surnames by N_countrynames sparse matrix
 cntrmat <- sparseMatrix(
   i = as.integer(cntr$surname),
-  j = as.integer(cntr$country),
+  j = as.integer(cntr$country2),
   x = as.integer(cntr$freq),
   dims = c(N_surnames, N_countrynames))
 rownames(cntrmat) = surnames
 colnames(cntrmat) = countrynames
-
-
-# What's the matter with "Nguyen"?
-sort(prop.table(cntrmat[which.max(rowSums(cntrmat)), ]))
-
-# What about "Lauderdale"?
-cntrmat[grep("LAUDERDALE", surnames), ]
 
 # Combine US, Terr, and Foreign matrices
 denommat <- cbind(ntvmat,terrmat,fbmat)
 
 # Create country proportion matrix
 gc()    # following command requires ~ 12GB working memory
-cntrmatprop <- sweep(cntrmat, 1, fbmat[, 1] + terrmat[, 1], FUN="/")
-gc()
+cntrmatprop <- sweep(cntrmat, 1, fbmat[, 1] + usmat[, 1], FUN="/")      # Originally take out the union of fb and terr. 
+gc()                                                                    # I think it makes more sense to take the union of fb and ntv.
 
 # Save objects
 save(cntrmat, cntrmatprop, 
@@ -102,3 +101,10 @@ save(cntrmat, cntrmatprop,
      surnames, N_surnames,
      countrynames, N_countrynames,
      file="data/AllData.Rdata")
+
+# Random tests
+# What's the matter with "Nguyen"?
+# sort(prop.table(cntrmat[which.max(rowSums(cntrmat)), ]))
+# What about "Lauderdale"?
+# cntrmat[grep("LAUDERDALE", surnames), ]
+
