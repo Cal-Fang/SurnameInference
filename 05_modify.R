4d15a5b51417746b6edade42e408bca830b537de
Cal Chengqi Fang <chengqi@uchicago.edu>
2022-05-03 18:29:27 -0500

    Updated the deterministic form output codes.

--- 05_modify.R
+++ 05_modify.R
@@ -13,31 +13,37 @@ setwd("~/Box Sync/Name Identification Project/US Names")    # Please change this
 load(file="data/result_1e-4.Rdata")
 
 # STEP 2
+result4_p <- result4_p[, surnames]
 result4p_df <- as.data.frame(t(result4_p))
-# Generate one set that has three 
-result4p_df2 <- round(result4p_df, 3)
 # write.csv(result4p_df2, "data/result4p_df2.csv")
+probability_df <- round(result4_p, 3)
 
 panasian_df <- result4p_df %>% 
-  mutate(asian = 1 - Other,
-         surname = rownames(result4)) %>% 
+  mutate(asian = 1 - Other) %>% 
+  round(3) %>% 
+  mutate(surname = rownames(result4p_df)) %>% 
   select(c("asian", "surname"))
-# panasian_df2 <- round(panasian_df, 3)
 
 # STEP 3
 # Create the deterministic form
 determine_df <- result4p_df %>%
-  mutate(largest = apply(result4_p, 2, max),
-         maxcntr_list = lapply(apply(t(result4_p), 1, function(x) which(x == max(x))), names),
-         surname = rownames(result4p_df)) %>%
+  mutate(largest = apply(result4_p, 2, max)) %>% 
+  round(3) %>%
+  mutate(maxcntr_list = lapply(apply(t(result4_p), 1, function(x) which(x == max(x))), names),
+         surname = rownames(result4p_df)) %>% 
   mutate(maxcntr = unlist(lapply(maxcntr_list, paste, collapse=" & "))) %>%
   select(-"maxcntr_list")
 
 # Mutate the second largest probability
-determine_df$largest2 <- apply(result4_p, 2, function(x) Rfast::nth(x, 2, descending=T))
+determine_df$largest2 <- apply(result4_p, 2, function(x) Rfast::nth(x, 2, descending=T)) %>% 
+  round(3)
+determine_df$maxcntr2 <- lapply(apply(t(result4_p), 1, function(x) which(x == Rfast::nth(x, 2, descending=T))), names)
+determine_df$maxcntr2[lengths(determine_df$maxcntr2) == 19] <- NA
+determine_df$maxcntr2 <- unlist(lapply(determine_df$maxcntr2, paste, collapse=" & "))
 
+determine_df <- determine_df[, c("surname", "maxcntr", "maxcntr2")]
 
-determine_df <- determine_df %>% 
-  filter(surname %in% fb_terr_ntv4.1$surname)
-
-
+save(probability_df,
+     panasian_df,
+     determine_df,
+     file="data/ReportResult.Rdata")
