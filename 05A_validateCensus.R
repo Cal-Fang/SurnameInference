91201823cf34faecf67f98541b7f5b5d305179d2
Cal Chengqi Fang <chengqi@uchicago.edu>
2024-06-26 15:15:35 -0500

    Add some validation codes.

---
+++ 05A_validateCensus.R
@@ -0,0 +1,184 @@
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##
+## Script name: 05A_validateCensus.R
+##
+## Purpose of script: To validate the results with census data.
+##
+## Author: Cal Chengqi Fang
+##
+## Date Created: 2024-06-07
+##
+## Copyright (c) Cal Chengqi Fang, 2023
+## Email: cal.cf@uchicago.edu
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##
+## Notes:
+##   1. We reconfigured the Hispanic percentage into four main racial groups based on data 
+## from the 2010 Census Brief, "Overview of Race and Hispanic Origin: 2010." This report 
+## indicated the following racial composition: 53% White, 2.5% Black, 1.4% Native American, 
+## 0.4% Asian, 36.7% some other race, and 6% two or more races.
+##   2. This script primarily validates our results' ability to predict the Asian racial group.
+##   3. 2010 Census surname list can be downloaded from the United States Census Bureau website:
+## https://www2.census.gov/topics/genealogy/2010surnames/names.zip
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## set working directory for Mac and PC
+setwd("/Users/atchoo/Documents/GitHub/Name-Method-Project/")     # Cal's working directory (mac)
+# setwd("C:/Users/")     # Cal's working directory (PC)
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+rm(list=ls())
+options(scipen=6, digits=4)         # I prefer to view outputs in non-scientific notation
+memory.limit(30000000)                  # this is needed on some PCs to increase memory allowance, but has no impact on macs.
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## load up the packages we will need:  (uncomment as required)
+require(tidyverse)
+require(data.table)
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------- STEP 1 ----------------------------------------------- 
+# Load in the Rdata file of reportable results
+load(file="data/interm/resultRep.Rdata")
+
+# Read in and modify the CENSUS
+census100 <- read_csv("data/namesCensus/Names_2010Census.csv") 
+census100[, 2:11] <- sapply(census100[, 2:11], as.numeric)
+
+# Load in the names dropped in the cleaning process
+load("results/tests/droppedNames.Rdata")
+
+
+# ----------------------------------------------- STEP 2 ----------------------------------------------- 
+# Modify every name to 12 letter length
+census100 <- census100 %>% 
+  mutate(name = ifelse((nchar(name) <= 12), 
+                       str_pad(name, 12, side="right", pad=" "), 
+                       str_trunc(name, 12, ellipsis=""))) 
+
+# Reconfigure the hispanic percentage to the four race group
+census100 <- census100 %>%
+  mutate(pctwhite = if_else(!is.na(pcthispanic), pctwhite + pcthispanic * 0.53, pctwhite),
+         pctblack = if_else(!is.na(pcthispanic), pctblack + pcthispanic * 0.025, pctblack),
+         pctapi = if_else(!is.na(pcthispanic), pctapi + pcthispanic * 0.004, pctapi),
+         pctaian = if_else(!is.na(pcthispanic), pctaian + pcthispanic * 0.014, pctaian),
+         pct2prace = if_else(!is.na(pcthispanic), pct2prace + pcthispanic * 0.06, pct2prace)) 
+
+# Create a variable denoting the highest proportion
+census100 <- census100 %>%
+  pivot_longer(cols=pctwhite:pctaian, names_to="maxRace", values_to="maxPct") %>%
+  group_by(name) %>%
+  filter(maxPct == max(maxPct)) %>%
+  ungroup() %>% 
+  select(name, maxPct) %>% 
+  distinct() %>% 
+  left_join(census100) %>% 
+  mutate(asianCensus = ifelse(!is.na(pct2prace) & pct2prace == maxPct, NA,
+                              ifelse(pctapi == maxPct, TRUE, FALSE))) %>% 
+  distinct(name, asianCensus) 
+
+
+# ----------------------------------------------- STEP 3 ----------------------------------------------- 
+# Create a probability of being Asian in our result
+probList <- probList %>% 
+  mutate(asian = 1 - Other)
+
+# Check the inclusion
+test_df <- census100 %>%
+  filter(asianCensus==TRUE) %>%
+  merge(probList, by.x="name", by.y="surname", all.x=TRUE)
+
+# Calculate the coverage rate (87.02%)
+sum(!is.na(test_df$asian)) / nrow(test_df)
+
+# Examine where the names were dropped
+noton <- filter(test_df, is.na(test_df$asian))
+noton1 <- noton[!(noton$name %in% fb_ntv$surname), ]
+noton2 <- noton[(noton$name %in% fb_ntv$surname) & !(noton$name %in% fb_ntv2$surname), ]
+noton3 <- noton[(noton$name %in% fb_ntv2$surname) & !(noton$name %in% fb_ntv3$surname), ]
+noton4 <- noton[(noton$name %in% fb_ntv3$surname) & !(noton$name %in% fb_ntv4$surname), ]
+noton5 <- noton[(noton$name %in% fb_ntv4$surname) & !(noton$name %in% fb_ntv5$surname), ]
+
+cat("Missingness status:\n", nrow(noton1), "names were never in our original data.\n",
+    nrow(noton2), "names were omitted because the total frequency in our data is not larger than 50.\n",
+    nrow(noton3), "names were omitted because the total 19 Asian countries foreign born frequency is not larger than 10.\n",
+    nrow(noton4), "names were omitted because the most foreign born were from outside Asia.\n",
+    nrow(noton5), "name was omitted because the foreign born rate were smaller than 5%.")
+
+# Remove objects to clean the memory
+rm(fb_ntv, fb_ntv2, fb_ntv3, fb_ntv4, fb_ntv5,
+   noton, noton1, noton2, noton3, noton4, noton5)
+
+
+# ----------------------------------------------- STEP 4 ----------------------------------------------- 
+# Examine the prediction of the whole list
+predict <- census100 %>% 
+  merge(probList, by.x="name", by.y="surname", all.x=TRUE) %>% 
+  mutate(TP = asianCensus==TRUE & asian > 0.50,
+         FP = asianCensus!=TRUE & asian > 0.50,
+         TN = asianCensus!=TRUE & (asian < 0.50 | is.na(asian)),
+         FN = asianCensus==TRUE & (asian < 0.50 | is.na(asian)))
+
+# Name count
+wholeCount <- sum(!is.na(predict$asian))
+# PPV
+wholePPV <- sum(predict$TP, na.rm=TRUE) / (sum(predict$TP, na.rm=TRUE) + sum(predict$FP, na.rm=TRUE))
+# Sensitivity
+wholeSensitivity <- sum(predict$TP, na.rm=TRUE) / (sum(predict$TP, na.rm=TRUE) + sum(predict$FN, na.rm=TRUE))
+# Specificity
+wholeSpecificity <- sum(predict$TN, na.rm=TRUE) / (sum(predict$TN, na.rm=TRUE) + sum(predict$FP, na.rm=TRUE))
+
+cat("Whole list prediction with ", wholeCount, " surnames has ",
+    "PPV of ", wholePPV, ", ",
+    "sensitivity of ", wholeSensitivity, ", and ",
+    "specificity of ", wholeSpecificity, ".",
+    sep="")
+
+# ----------------------------------------------- STEP 5 ----------------------------------------------- 
+# Create a variable to denote the most probable origin
+## Here we report the most probable origin among the 19 Asian area
+cntr <- probList %>% 
+  select(-Other) %>% 
+  pivot_longer(cols=AF:XK, names_to="maxCntr", values_to="maxProp") %>%
+  group_by(surname) %>%
+  filter(maxProp == max(maxProp)) %>%
+  ungroup() %>% 
+  select(surname, maxCntr, asian)
+
+# Repeat the process
+## We have to repeat the process instead of creating maxCntr before STEP 3
+## because there are surnames that fall into multiple maxCntr category.
+predictSub <- census100 %>% 
+  merge(cntr, by.x="name", by.y="surname", all.x=TRUE) %>% 
+  mutate(TP = asianCensus==TRUE & asian > 0.50,
+         FP = asianCensus!=TRUE & asian > 0.50,
+         TN = asianCensus!=TRUE & (asian < 0.50 | is.na(asian)),
+         FN = asianCensus==TRUE & (asian < 0.50 | is.na(asian))) %>% 
+  select(name, maxCntr, 
+         TP, FP, TN, FN)
+
+# Examine the prediction of the sub-lists
+predictSubSummary <- predictSub %>% 
+  group_by(maxCntr) %>% 
+  summarise(nameCount = n(),
+            PPV = sum(TP, na.rm=TRUE) / (sum(TP, na.rm=TRUE) + sum(FP, na.rm=TRUE)),
+            Sensitivity = NA,
+            Specificity = NA) %>% 
+  filter(!is.na(maxCntr))
+
+
+# ----------------------------------------------- STEP 5 ----------------------------------------------- 
+# Combine the sub-list evaluation results with the result of the whole list
+predictSummary <- rbind(c("Whole List", wholeCount, wholePPV, wholeSensitivity, wholeSpecificity),
+                        predictSubSummary)
+
+# Save the results
+save(predictSummary,
+     file="data/interm/validateCensus.Rdata")
+write_csv(predictSummary, 
+          file="results/validate/validateResultsCensus.csv")
---
+++ 05B_validateCV.R
@@ -0,0 +1,233 @@
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##
+## Script name: 
+##
+## Purpose of script: 
+##
+## Author: Cal Chengqi Fang
+##
+## Date Created: 2024-06-07
+##
+## Copyright (c) Cal Chengqi Fang, 2024
+## Email: cal.cf@uchicago.edu
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##
+## Notes:
+##   
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## set working directory for Mac and PC
+setwd("/Users/atchoo/Documents/GitHub/Name-Method-Project/")     # Cal's working directory (mac)
+# setwd("C:/Users/")     # Cal's working directory (PC)
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+rm(list=ls())
+options(scipen=6, digits=4)         # I prefer to view outputs in non-scientific notation
+memory.limit(30000000)                  # this is needed on some PCs to increase memory allowance, but has no impact on macs.
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## load up the packages we will need:  (uncomment as required)
+require(tidyverse)
+require(data.table)
+require(Matrix)
+require(caret)        # For the confusion matrix estimation function
+require(foreach)      # For the parallel computing 
+require(doParallel)   # For the parallel computing
+require(parallel)     # For the parallel computing
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------- STEP 1 ----------------------------------------------- 
+# Load the cleaned data
+load("data/interm/cleaned.Rdata")
+
+
+# ----------------------------------------------- STEP 2 ----------------------------------------------- 
+# Filter out the surnames of interest
+cntr <- cntr %>% 
+  filter(surname %in% surnames)
+ntv <- ntv %>% 
+  filter(surname %in% surnames)
+
+# Expand the data
+cntr <- cntr %>%
+  uncount(freq) 
+cntr <- cntr %>%
+  mutate(id = 1:nrow(cntr))
+
+# Define number of folds
+k <- 4
+
+# Create the folds
+set.seed(20240607)
+folds <- createFolds(cntr$id, k=k, list=TRUE)
+
+
+# ----------------------------------------------- STEP 3 ----------------------------------------------- 
+# Make a function for EM algorithm
+run_EM <- function(M, N, Y, Y0, critv){
+  P <- matrix(1 / (M * N), nrow=N, ncol=M)
+  P_new <- matrix(NA, nrow=N, ncol=M)
+  
+  for (i in 1:N) {
+    for (j in 1:M) {
+      P_new[i, j] <- Y[i, j] + Y0[j] * P[i, j] / sum(P[, j])
+    }
+  }
+  
+  P_new <- P_new / sum(P_new)
+  diff <- max(abs(P_new - P))
+  
+  P <- P_new
+  
+  while (diff > critv) {
+    for (i in 1:N) {
+      for (j in 1:M) {
+        P_new[i,j] <- Y[i,j] + Y0[j] * P[i, j] / sum(P[, j])
+      }
+    }
+    
+    P_new <- P_new / sum(P_new)
+    diff <- max(abs(P_new - P))
+    
+    print(diff)
+    
+    P <- P_new
+  }
+  
+  return(P_new)
+}
+
+# Make a function for cross-validation
+run_fold <- function(train_data, test_data, supplement=ntv) {
+  # Drop the surnames where only the other category has non-NA values
+  keep <- train_data %>% 
+    filter(new_code != "Other") %>% 
+    select(surname) %>% 
+    distinct()
+  
+  train_data <- train_data %>% 
+    filter(surname %in% keep$surname)
+  
+  # Collapse the train sets back to summary level to run EM
+  train_data <- train_data %>% 
+    group_by(new_code, surname) %>% 
+    summarise(freq = n(), .groups = 'drop')
+  
+  # Prepare the matrix
+  surnames <- sort(unique(train_data$surname))
+  M <- length(surnames)        
+  
+  train_data$surname <- factor(train_data$surname, levels=surnames)
+  ntv$surname <- factor(ntv$surname, levels=surnames)
+  
+  # Create vector of country codes 
+  countrynames <- sort(unique(train_data$new_code))
+  N <- length(countrynames)
+  
+  train_data$new_code <- factor(train_data$new_code, levels=countrynames)
+  
+  # Create US born column sparse matrix
+  ntvmat <- sparseMatrix(
+    i = as.integer(ntv$surname[!is.na(ntv$surname)]),
+    j = rep(1, sum(!is.na(ntv$surname))),
+    x = as.integer(ntv$freq[!is.na(ntv$surname)]),
+    dims = c(M,1))
+  rownames(ntvmat) <- surnames
+  colnames(ntvmat) <- "US"
+  
+  # Create M by N sparse matrix
+  train_data <- train_data %>%
+    filter(!is.na(surname))
+  trainmat <- sparseMatrix(
+    i = as.integer(train_data$surname),
+    j = as.integer(train_data$new_code),
+    x = as.integer(train_data$freq),
+    dims = c(M, N))
+  rownames(trainmat) <- surnames
+  colnames(trainmat) <- countrynames
+  
+  # Rotate all matrices needed for further analysis
+  trainmat <- t(trainmat)
+  ntvmat <- t(ntvmat)
+  
+  # Run EM on train data
+  result_train <- run_EM(M, N, trainmat, ntvmat, 1e-4)
+  
+  # Create the conditional probability form result with Bayes' theorem
+  probList_train <- t(result_train)/colSums(result_train)
+  rownames(probList_train) <- surnames
+  colnames(probList_train) <- countrynames
+  
+  probList_train <- as_tibble(probList_train) %>% 
+    round(4) %>% 
+    mutate(surname = surnames)
+  
+  # Create the deterministic form result
+  determList_train <- probList_train %>% 
+    pivot_longer(cols=AF:XK, names_to="area", values_to="probability") %>% 
+    filter(probability >= 0.5) 
+  
+  # Attach the deterministic prediction result to the test set
+  predict_result <- test_data %>% 
+    select(-country) %>% 
+    merge(determList_train) %>% 
+    select(-probability) %>% 
+    mutate(expected = factor(new_code, levels=countrynames),
+           predicted = factor(area, levels=countrynames))
+  
+  # Evaluate the prediction
+  evalute_predict <- confusionMatrix(data=predict_result$predicted, 
+                                     reference=predict_result$expected)
+  
+  evaluate_predict <- evalute_predict[[4]] %>% 
+    as_tibble() %>% 
+    mutate(area = countrynames)
+  
+  return(evaluate_predict)
+}
+
+
+# ----------------------------------------------- STEP 4 ----------------------------------------------- 
+# Setup parallel backend to use many processors
+num_cores <- detectCores() - 1
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+
+# Run cross-validation in parallel
+cvResults <- foreach(fold=folds, 
+                     .combine='bind_rows', 
+                     .packages=c('tidyverse', 'Matrix', 'caret', 'data.table')) %dopar% {
+                       train_data <- cntr %>% 
+                         filter(!(id %in% fold))
+                       test_data <- cntr %>% 
+                         filter(id %in% fold)
+                       run_fold(train_data, test_data, supplement=ntv)
+                      }
+
+# Stop parallel processing
+stopCluster(cl)
+registerDoSEQ()
+
+
+# ----------------------------------------------- STEP 5 -----------------------------------------------
+# Attach the fold number
+cvResults <- cvResults %>% 
+  mutate(fold = rep(1:4, each=20)) 
+
+# Create a summary file
+cvResultsSummary <- cvResults %>% 
+  group_by(area) %>% 
+  summarise(across(.cols=Sensitivity:`Balanced Accuracy`, 
+                   .fns=~mean(.x, na.rm=TRUE)))
+
+# Save the results
+save(cvResults, cvResultsSummary,
+     file="data/interm/validateCV.Rdata")
+write_csv(cvResultsSummary, 
+          file="results/validate/validateResultsCV.csv")
+
